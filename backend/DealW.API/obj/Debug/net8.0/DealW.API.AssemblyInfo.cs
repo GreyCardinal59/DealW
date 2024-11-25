ac107275032b9f29973873a928978715a174d8cb
GreyCardinal59 <grey59cardinal@gmail.com>
2024-11-26 03:16:51 +0500

    initial commit

--- backend/DealW.API/obj/Debug/net8.0/DealW.API.AssemblyInfo.cs
+++ backend/DealW.API/obj/Debug/net8.0/DealW.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DealW.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ad74612bedad42f4e989907e9cb68a079643ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DealW.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DealW.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
